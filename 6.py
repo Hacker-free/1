6285b3fdf066dd2ef1e85e158ec43f22045c5a61
Hacker-free <112991346+Hacker-free@users.noreply.github.com>
2022-09-07 20:19:14 +0100

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,22 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('clear')
+    os.system ('figlet Hk Google ')
+    time.sleep(0.2)
+    fx9 = input (" \033[1;33m @ نودﺑ ﺔﯾﺤﺿ بﺎﺴﺤ مﺴإ  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == 'jlaibi1soifiane' :
+            os.system ('python g.py')
+main (main)
+home_main ()
---
+++ g.py
@@ -0,0 +1,42 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('clear')
+    os.system ('figlet Hk Google')
+    time.sleep(0.2)
+    print ("")
+    time.sleep(0.2)
+    print ('   \033[1;33m{\033[1;36m1\033[1;33m}\033[1;32m Gmail')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;33m{\033[1;36m2\033[1;33m}\033[1;32m Hotmail')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;33m{\033[1;36m3\033[1;33m}\033[1;32m Yahoo')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;33m{\033[1;36m4\033[1;33m}\033[1;32m Outlook')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;33m{\033[1;36m5\033[1;33m}\033[1;32m Office')
+    time.sleep(1)
+    fx9 = input (" \033[1;33mjlaibi1soufiane  @ => \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == '1' :
+            os.system ('python root2.py')
+main (main)
+home_main ()
